2dc794b4e31ac45f3d0642e6d2e094f4ae487303
Zach Quinn <zacharytq@gmail.com>
2021-11-25 17:12:05 -0500

    made event model, migrated, started graphql

--- app/app/settings/base.py
+++ app/app/settings/base.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'events.apps.EventsConfig',
+    'graphene_django'
 ]
 
 MIDDLEWARE = [
--- app/events/models.py
+++ app/events/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    start_date_time = models.DateTimeField()
+    end_date_time = models.DateTimeField(blank=True)
+    created_date_time  = models.DateTimeField(auto_now_add=True)
+    all_day = models.BooleanField()
+    location = models.CharField(max_length=200, blank=True)
+    description = models.CharField(max_length=500, blank=True)
+
+    def __str__(self):
+        return self.title
